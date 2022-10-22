6d6cae3fb6cf27020c6d6da60942503bcb275560
Алексей <pirat6666@gmail.com>
2022-10-22 21:11:30 +0300

    Задача2

---
+++ task2/Program.cs
@@ -0,0 +1,24 @@
+﻿/* Задача 10: Напишите программу, которая принимает на вход 
+трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+456 -> 5
+782 -> 8
+918 -> 1 */
+
+Console.WriteLine("Введите трехзначное число.");
+    while(true)
+    {
+        string nambers = Console.ReadLine();
+        if(nambers.Length == 3 )
+        {
+            Console.WriteLine( nambers[1]);
+            
+        }
+        else
+        {
+            Console.WriteLine("Ошибка!! Введите трехзначное число.");
+        }
+                    
+        break;
+    }        
+
